933caa79e92b3863a5c6b9ec7628cd697f4552ae
oas1s <zll040519@outlook.com>
2025-04-06 17:35:39 +0800

    finish alg5~10

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -2,7 +2,7 @@
 	queue
 	This question requires you to use queues to implement the functionality of the stac
 */
-// I AM NOT DONE
+
 
 #[derive(Debug)]
 pub struct Queue<T> {
@@ -24,14 +24,14 @@ impl<T> Queue<T> {
         if !self.elements.is_empty() {
             Ok(self.elements.remove(0usize))
         } else {
-            Err("Queue is empty")
+            Err("Stack is empty")
         }
     }
 
     pub fn peek(&self) -> Result<&T, &str> {
         match self.elements.first() {
             Some(value) => Ok(value),
-            None => Err("Queue is empty"),
+            None => Err("Stack is empty"),
         }
     }
 
@@ -52,30 +52,40 @@ impl<T> Default for Queue<T> {
     }
 }
 
-pub struct myStack<T>
-{
-	//TODO
-	q1:Queue<T>,
-	q2:Queue<T>
+pub struct myStack<T> {
+    q1: Queue<T>,
+    q2: Queue<T>,
 }
+
 impl<T> myStack<T> {
     pub fn new() -> Self {
         Self {
-			//TODO
-			q1:Queue::<T>::new(),
-			q2:Queue::<T>::new()
+            q1: Queue::<T>::new(),
+            q2: Queue::<T>::new(),
         }
     }
+
     pub fn push(&mut self, elem: T) {
-        //TODO
+        // 将新元素添加到 q2
+        self.q2.enqueue(elem);
+
+        // 将 q1 中的所有元素移动到 q2
+        while let Ok(value) = self.q1.dequeue() {
+            self.q2.enqueue(value);
+        }
+
+        // 交换 q1 和 q2，使 q1 始终是主队列
+        std::mem::swap(&mut self.q1, &mut self.q2);
     }
+
     pub fn pop(&mut self) -> Result<T, &str> {
-        //TODO
-		Err("Stack is empty")
+        // 从 q1 中移除并返回队首元素
+        self.q1.dequeue()
     }
+
     pub fn is_empty(&self) -> bool {
-		//TODO
-        true
+        // 检查 q1 是否为空
+        self.q1.is_empty()
     }
 }
 
